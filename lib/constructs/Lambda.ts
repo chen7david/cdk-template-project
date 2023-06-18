6cf62e252876d0fce0c5c41ad8d4c053d36d9030
David M. Chen <chen7david@me.com>
2023-06-18 11:50:06 -0300

    fixed cdk deploy commands

--- lib/constructs/Lambda.ts
+++ lib/constructs/Lambda.ts
@@ -1,6 +1,5 @@
 import { Construct } from 'constructs'
-import { aws_lambda as CdkLambda } from 'aws-cdk-lib'
-import { FunctionProps, Runtime } from 'aws-cdk-lib/aws-lambda'
+import { FunctionProps, Runtime, Function, Code } from 'aws-cdk-lib/aws-lambda'
 import * as path from 'path'
 
 interface CustomProps extends FunctionProps {
@@ -9,12 +8,12 @@ interface CustomProps extends FunctionProps {
   handler: string
 }
 
-export class Lambda extends CdkLambda.Function {
+export class Lambda extends Function {
   constructor(scope: Construct, id: string, props?: Partial<CustomProps>) {
     super(scope, id, {
       functionName: `project-${props?.name || 'name'}`,
       runtime: Runtime.NODEJS_18_X,
-      code: CdkLambda.Code.fromAsset(
+      code: Code.fromAsset(
         path.join(__dirname, `../../dist/${props?.folder || 'handlers'}`)
       ),
       handler: `${props?.handler || 'index.handler'}`,
